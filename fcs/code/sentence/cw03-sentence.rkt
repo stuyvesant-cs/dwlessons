635a3d627878795e4469598a8e0da0a2d64be88e
JonAlf <jonalf@gmail.com>
2023-11-01 14:01:11 -0400

    a

--- fcs/code/sentence/cw03-sentence.rkt
+++ fcs/code/sentence/cw03-sentence.rkt
@@ -1,53 +1,56 @@
 #lang racket
-
-
+#|
+You can leave these variable definitions, and the
+definitions of nth  and randElemnt alone.
+|#
 (define articles (list "the" "one" "a" "an"))
 (define nouns (file->lines "nouns.txt"))
 (define verbs (file->lines "verbs.txt"))
 (define adjectives (file->lines "adjectives.txt"))
 
-;;list processing functions
+;list processing functions
 (define nth
   (lambda (n g)
     (cond
       ((= n 0) (car g))
       (else (nth (- n 1) (cdr g))))))
-
 (define randElement
   (lambda (g)
     (if (null? g)
         '()
         (nth (random (length g)) g))))
 
-
-;;sentence generator specific functions
+#|
+Sentence generator specific functions
+each of these will return a random word of the
+specified part of speech.
+You should leave these alone as well.
+|#
 (define noun (lambda () (randElement nouns)))
 (define verb (lambda () (randElement verbs)))
 (define article (lambda () (randElement articles)))
 (define adjective (lambda () (randElement adjectives)))
 
+
+;Task 1 Question 0
 (define wordy-noun
   (lambda ()
-    (if (< 2 (random 5))
-        (list (noun))
-        (cons (adjective) (wordy-noun)))))
+     (list (noun))))
 
-(define subject
+;Task 1 Question 1
+(define noun-phrase
   (lambda ()
-    (if (= 0 (random 4))
-        (cons (article) (wordy-noun))
-        (wordy-noun))))
+    (wordy-noun)))
 
+;Task 1 Question 2
 (define verb-phrase
   (lambda ()
-    (if (= 0 (random 2))
-        (cons (verb) (subject))
-        (list (verb)))))
+    (list (verb))))
            
 
 (define sentence
   (lambda ()
-    (string-join (append (subject) (verb-phrase)))))
+    (string-join (append (noun-phrase) (verb-phrase)))))
 
 (sentence)
 
